ff0dc662089e880d74c5e3692b6bbc3dba191a08
luis-reyes <exfinge@gmail.com>
2023-06-06 10:24:13 -0400

    created neww react app with a gitactions workflow

---
+++ __tests__/addition.test.tsx
@@ -0,0 +1,10 @@
+import 'react-native';
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+
+test('1 + 1 equals 2', () => {
+    const result = 1 + 1;
+    expect(result).toBe(2);
+  });
+  
